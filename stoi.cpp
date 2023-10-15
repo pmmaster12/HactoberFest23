cdcc2effb2e469510a1d7d653ee1cbc52b490c23
pmmaster12 <120505111+pmmaster12@users.noreply.github.com>
2023-10-16 01:12:05 +0530

    Create stoi.cpp

---
+++ stoi.cpp
@@ -0,0 +1,26 @@
+// C++ Program to convert
+// string into integer
+// Using stoi
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	string str = "2009, GeeksforGeeks_founded";
+	string str1 = "0x6C1";
+	string str2 = "-10010010101";
+
+	// Calling stoi() for all strings.
+	int num = stoi(str);
+	int num_hex = stoi(str1, nullptr, 16);
+	int num_bin = stoi(str2, nullptr, 2);
+
+	// printing converted values
+	cout << str << ": " << num << endl;
+	cout << str1 << ": " << num_hex << endl;
+	cout << str2 << ": " << num_bin << endl;
+
+	return 0;
+}
